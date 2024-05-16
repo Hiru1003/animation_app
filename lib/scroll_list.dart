a514bf649cfed8db41b68e59deb781301321185a
Hiru1003 <hirumiperera10@gmail.com>
2024-05-16 15:57:07 +0530

    feat: scaffold home

---
+++ lib/scroll_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomepagState extends StatefulWidget {
+  const HomepagState({super.key});
+
+  @override
+  State<HomepagState> createState() => __HomepagStateState();
+}
+
+class __HomepagStateState extends State<HomepagState> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
